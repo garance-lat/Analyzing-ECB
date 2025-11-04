c18eea6e4ee50df9671d5e4559fadad67299e139
Garance Latieule <garance.latieule@ensae.fr>
2025-11-04 14:08:46 +0100

    tooling: add data reconstruction script

---
+++ scripts/reconstruct_data.ps1
@@ -0,0 +1,6 @@
+﻿# Reconstruit les CSV à partir des .partXX (Windows PowerShell)
+Get-Content -Path dataset\pre_processed_ECB.csv.part?? -Raw -Encoding Byte |
+  Set-Content -Path dataset\pre_processed_ECB.csv -Encoding Byte
+
+Get-Content -Path dataset\pre_processed_ECB_clean_simple.csv.part?? -Raw -Encoding Byte |
+  Set-Content -Path dataset\pre_processed_ECB_clean_simple.csv -Encoding Byte
